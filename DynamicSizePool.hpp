b99ee7f04d7dcdf1dfa77884a14a923b26630bfc
Johann Dahm <johann.dahm@gmail.com>
2018-05-16 16:53:40 -0700

    Revert "splitBlock alignment fix from @mcfadden8"
    This reverts commit 8cfa9942ae626a25e3df57031924374f1ee77182.
    
    Causes the size tracking to break and thus the unit tests to fail.

--- DynamicSizePool.hpp
+++ DynamicSizePool.hpp
@@ -47,14 +47,9 @@ protected:
     }
   }
 
-  inline std::size_t alignmentAdjust(const std::size_t size) {
-    const std::size_t AlignmentBoundary = 16;
-    return std::size_t (size + (AlignmentBoundary-1)) & ~(AlignmentBoundary-1);
-  }
-
   // Allocate a new block and add it to the list of free blocks
   void allocateBlock(struct Block *&curr, struct Block *&prev, const std::size_t size) {
-    const std::size_t sizeToAlloc = std::max(alignmentAdjust(size), minBytes);
+    const std::size_t sizeToAlloc = std::max(size, minBytes);
     curr = prev = NULL;
     void *data = NULL;
 
@@ -84,9 +79,7 @@ protected:
 
   void splitBlock(struct Block *&curr, struct Block *&prev, const std::size_t size) {
     struct Block *next;
-    const std::size_t alignedsize = alignmentAdjust(size);
-
-    if ( curr->size == size || curr->size == alignedsize ) {
+    if ( curr->size == size ) {
       // Keep it
       next = curr->next;
     }
